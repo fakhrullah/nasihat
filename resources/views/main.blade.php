158342b1731765c649e68a03ab11d4d6b6664b8a
fakhrul <fakhrullah@fajarhac.com>
2015-08-09 16:25:13 +0800

    add mobile overview

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>{{$title}}</title>
+        <meta name=viewport content="width=device-width, initial-scale=1">
     </head>
     
     <body style="margin: 0;padding: 0;">
