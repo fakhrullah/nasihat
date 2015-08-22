3b010ed73e1a7ddcbb47db9e1b04f13cfced5767
fakhrul <fakhrullah@fajarhac.com>
2015-08-22 16:34:57 +0800

    add seeder for backgrounds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder {
         Model::unguard();
 
         // $this->call('UserTableSeeder');
-         $this->call('QuoteTableSeeder');
+//         $this->call('QuoteTableSeeder');
+         $this->call('BackgroundsTableSeeder');
     }
 
 }
